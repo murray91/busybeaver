8cd62df3f79e503c6e48a977b208d79667b8c09b
murray91 <74630349+murray91@users.noreply.github.com>
2020-11-29 00:31:34 +0100

    fresh start

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='busybeaver',
+      version='0.0.1',
+      description='Simplifies post-processing of model results into GIS formats.',
+      url='https://github.com/murray91/beaver',
+      author='Ryan Murray',
+      author_email='ryanwaltermurray@gmail.com',
+      license='MIT',
+      packages=['busybeaver'],
+      zip_safe=False
+)

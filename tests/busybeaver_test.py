915f4806c9819abfe8b1deeef943597d4dff1a69
murray91 <ryanwaltermurray@gmail.com>
2020-11-30 00:38:34 +0100

    fix test

--- tests/busybeaver_test.py
+++ tests/busybeaver_test.py
@@ -2,13 +2,10 @@ import busybeaver as bb
 import os
 
 FILE_DIR = os.path.dirname(__file__)
+config_file = os.path.join(FILE_DIR, r"data\test_config.ini")
 
-# Hut tests
-def test_Hut_initialize():
-
-    print(FILE_DIR)
-    config_file = os.path.join(FILE_DIR, r"data\test_config.ini")
+# Hut initialization tests
+def test_Hut_initialize_models():
     testhut = bb.Hut(config_file)
-    print(testhut.config_file)
-
     assert testhut.models == ["newModel1", "newModel2"]
+
